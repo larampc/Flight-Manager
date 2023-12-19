21cfec99bbd74aa32ebc69d41e8dbddd861bf7d7
Lara <larapc2004@gmail.com>
2023-12-19 11:55:05 +0000

    Created Airline

---
+++ FlightManager/Airline.cpp
@@ -0,0 +1,17 @@
+#include "Airline.h"
+
+std::string Airline::getCode() const {
+    return code;
+}
+
+std::string Airline::getName() const {
+    return name;
+}
+
+std::string Airline::getCallSign() const {
+    return callSign;
+}
+
+std::string Airline::getCountry() const {
+    return country;
+}
---
+++ FlightManager/Airline.h
@@ -0,0 +1,22 @@
+#ifndef AEDPROJ2_AIRLINE_H
+#define AEDPROJ2_AIRLINE_H
+
+#include <string>
+
+class Airline {
+private:
+    std::string code;
+    std::string name;
+    std::string callSign;
+    std::string country;
+public:
+    Airline(std::string code, std::string name, std::string callSign, std::string country):
+    code(code), name(name), callSign(callSign), country(country) {};
+    std::string getCode() const;
+    std::string getName() const;
+    std::string getCallSign() const;
+    std::string getCountry() const;
+};
+
+
+#endif //AEDPROJ2_AIRLINE_H
